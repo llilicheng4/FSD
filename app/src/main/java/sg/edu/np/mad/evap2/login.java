d3dacd1cb383273fb887183100185f27698bbd27
Jing Jie Wang <s10198179@connect.np.edu.sg>
2020-11-11 10:40:05 +0800

    sign up should work with firebase

--- app/src/main/java/sg/edu/np/mad/evap2/login.java
+++ app/src/main/java/sg/edu/np/mad/evap2/login.java
@@ -5,7 +5,7 @@ import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class login extends AppCompatActivity {
-
+    public static UserData userdata;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
--- app/src/main/java/sg/edu/np/mad/evap2/signup.java
+++ app/src/main/java/sg/edu/np/mad/evap2/signup.java
@@ -23,12 +23,90 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
 public class signup extends AppCompatActivity {
-    
+    private EditText etUsername, etEmail, etPwd, etCfmPwd;
+    private Button btnSignUp;
+    private FirebaseAuth mAuth;
+    private DatabaseReference ref;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.signup);
 
+        etUsername = findViewById(R.id.etUsername);
+        etEmail = findViewById(R.id.etEmail);
+        etPwd = findViewById(R.id.etPassword);
+        etCfmPwd = findViewById(R.id.etCfmpassword);
 
+        btnSignUp = findViewById(R.id.btnSignup);
+
+        mAuth = FirebaseAuth.getInstance();
+        ref = FirebaseDatabase.getInstance().getReference();
+
+        //submit account data into firebase when button is clicked
+        btnSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final String username = etUsername.getText().toString();
+                final String email = etEmail.getText().toString();
+                final String pwd = etPwd.getText().toString();
+                String Cfmpwd = etCfmPwd.getText().toString();
+
+                //checks if the field is empty
+                if(username.isEmpty() || email.isEmpty() || pwd.isEmpty() || Cfmpwd.isEmpty()){
+                    Toast.makeText(signup.this, "Please complete all the details.", Toast.LENGTH_SHORT).show();
+                }
+                //checks if both pwd field is the same
+                else if(!etPwd.getText().toString().equals(etCfmPwd.getText().toString())){
+                    Toast.makeText(signup.this, "Password must be the same.", Toast.LENGTH_SHORT).show();
+                }
+                else{
+                    final UserData userdata = new UserData(username, email, pwd);
+                    mAuth.createUserWithEmailAndPassword(email, pwd)
+                            .addOnCompleteListener(signup.this, new OnCompleteListener<AuthResult>() {
+                                @Override
+                                public void onComplete(@NonNull Task<AuthResult> task) {
+                                    if(task.isSuccessful()){
+                                        FirebaseUser user = mAuth.getCurrentUser();
+
+                                        writeNewStudent(user.getUid(), username, email, pwd);
+                                        Toast.makeText(signup.this, "Sign up successful", Toast.LENGTH_SHORT).show();
+                                        login.userdata = userdata;
+                                        Intent mainpage = new Intent(signup.this, login.class);
+                                        startActivity(mainpage);
+                                    }
+
+                                    else{
+                                        try {
+                                            throw task.getException();
+                                        } catch (FirebaseAuthWeakPasswordException weakPassword) {
+                                            Toast.makeText(signup.this, "You need at least 6 characters.",
+                                                    Toast.LENGTH_SHORT).show();
+
+                                        } catch (FirebaseAuthInvalidCredentialsException invalidEmail) {
+                                            Toast.makeText(signup.this, "Invalid email.",
+                                                    Toast.LENGTH_SHORT).show();
+                                        } catch (FirebaseAuthUserCollisionException emailExists) {
+                                            Toast.makeText(signup.this, "Email already exists.",
+                                                    Toast.LENGTH_SHORT).show();
+                                        } catch (Exception e) {
+                                            //Everything else
+                                        }
+                                        // If sign in fails, display a message to the user.
+                                        //Log.v(TAG, "createUserWithEmail:failure", task.getException());
+                                        Toast.makeText(signup.this, "Authentication failed.",
+                                                Toast.LENGTH_SHORT).show();
+                                    }
+                                }
+                            });
+                }
+            }
+        });
+    }
+
+    private void writeNewStudent(String userid, String username, String email, String pwd){
+        ref.child("users").child(userid).child("username").setValue(username);
+        ref.child("users").child(userid).child("email").setValue(email);
+        ref.child("users").child(userid).child("password").setValue(pwd);
     }
 }

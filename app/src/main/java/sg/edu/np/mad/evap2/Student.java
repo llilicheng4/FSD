de17d7ed669d749e012951e3f0a2623f53fb58cc
llilicheng4 <llilicheng4@gmail.com>
2020-11-13 00:20:37 +0800

    added Student, Module, Timetable class

---
+++ app/src/main/java/sg/edu/np/mad/evap2/Student.java
@@ -0,0 +1,67 @@
+package sg.edu.np.mad.evap2;
+
+public class Student extends UserData {
+private String StudentName, ClassID;
+private TimeTable timeTable;
+
+public Student(){};
+
+Student(String studentName, String classID){
+    StudentName = studentName;
+    ClassID = classID;
+}
+
+    @Override
+    public String getEmail() {
+        return super.getEmail();
+    }
+
+    @Override
+    public void setEmail(String email) {
+        super.setEmail(email);
+    }
+
+    @Override
+    public String getPassword() {
+        return super.getPassword();
+    }
+
+    @Override
+    public void setPassword(String password) {
+        super.setPassword(password);
+    }
+
+    @Override
+    public String getUsername() {
+        return super.getUsername();
+    }
+
+    @Override
+    public void setUsername(String username) {
+        super.setUsername(username);
+    }
+
+    public String getClassID() {
+        return ClassID;
+    }
+
+    public void setClassID(String classID) {
+        ClassID = classID;
+    }
+
+    public String getStudentName() {
+        return StudentName;
+    }
+
+    public void setStudentName(String studentName) {
+        StudentName = studentName;
+    }
+
+    public TimeTable getTimeTable() {
+        return timeTable;
+    }
+
+    public void setTimeTable(TimeTable timeTable) {
+        this.timeTable = timeTable;
+    }
+}

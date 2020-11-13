806156291d4142b39f751aa036466f2fc029ae90
Jing Jie Wang <s10198179@connect.np.edu.sg>
2020-11-13 10:33:22 +0800

    added discussion board and account xml and java fle (empty)

---
+++ app/src/main/java/sg/edu/np/mad/evap2/account.java
@@ -0,0 +1,13 @@
+package sg.edu.np.mad.evap2;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class account extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.account);
+    }
+}
---
+++ app/src/main/java/sg/edu/np/mad/evap2/discussionboard.java
@@ -0,0 +1,13 @@
+package sg.edu.np.mad.evap2;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class discussionboard extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.discussion_board);
+    }
+}
